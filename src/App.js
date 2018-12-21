42ab4c2dfc11f025541bdef43510b85890fdba99
ayn <vincentayndecastro@gmail.com>
2018-12-20 18:44:54 -0600

    reconfig

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
-import React from "react";
-import Clicky from "./components/Clicky";
+import React, { Component } from "react";
+import FriendCard from "./components/CarCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
 
-const App = () => <Clicky />;
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends
+  };
+
+  
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Car List</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            image={friend.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
 
 export default App;
